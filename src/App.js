be586af21f839d87b209c36eeefa3988c89aedb0
Sevagen Veerasamy <sevagen.veerasamy@hangarww.com>
2022-03-29 15:08:31 +0400

    fix: merge calculator component to app file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 
-import Calculator from "./components/Calculator";
+import Keypad from "./components/Keypad";
+import Result from "./components/Result";
+
+import styled from "styled-components";
 
 function App() {
-  return <Calculator />
+  const [displayNum, setDisplayNum] = useState(0);
+  const [current, setCurrent] = useState(0);
+  const accumulator = useRef([]);
+  return(
+    <Wrapper>
+      <Result displayNum={displayNum} current={current} />
+      <Keypad
+        setDisplayNum={setDisplayNum}
+        accumulator={accumulator}
+        setCurrent={setCurrent}
+      />
+    </Wrapper>
+    )
 }
+
+const Wrapper = styled.section`
+position: absolute;
+top: 50%;
+left: 50%;
+transform: translate(-50%, -50%);
+border: 1px solid black;
+border-radius: 5px;
+box-shadow: rgba(0, 0, 0, 0.56) 0px 22px 70px 4px;
+transition: box-shadow 0.5s ease-in-out;
+:hover {
+	box-shadow: rgba(17, 17, 26, 0.1)
+	0px 8px 24px, rgba(17, 17, 26, 0.1)
+	0px 16px 56px, rgba(17, 17, 26, 0.1)
+	0px 24px 80px;
+}
+`;
 
 export default App;
